fc626363368b64cb3a9a72279a03b0039e91c1d0
zhuyuanmin <13058039407@163.com>
2020-08-12 14:55:31 +0800

    Update config-overrides.js

--- config-overrides.js
+++ config-overrides.js
@@ -54,17 +54,6 @@ module.exports = override(
         if (process.env.NODE_ENV === 'production') config.devtool = false
         if (process.env.NODE_ENV !== 'development')
             config.plugins = [...config.plugins, ...myPlugin]
-        //1.修改、添加loader 配置 :
-        // 所有的loaders规则是在config.module.rules(数组)的第二项
-        // 即：config.module.rules[2].oneof  (如果不是，具体可以打印 一下是第几项目)
-        // 修改 sass 配置 ，规则 loader 在第五项(具体看配置)
-        const loaders = config.module.rules.find(rule => Array.isArray(rule.oneOf)).oneOf
-        loaders[5].use.push({
-            loader: 'sass-resources-loader',
-            options: {
-                resources: path.resolve(__dirname, 'src/asset/base.scss'), //全局引入公共的scss 文件
-            },
-        })
 
         //！ 这里尽量不要以赋值的方式覆盖原有配置
         config.output.path = path.resolve(__dirname, 'dist')
