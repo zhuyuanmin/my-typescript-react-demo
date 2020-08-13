484ebb36133d046787b0dc46d8a09f54010c975e
zhuyuanmin <13058039407@163.com>
2020-08-13 10:43:40 +0800

    Update index.ts

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,7 @@
-import counter from '@/store/counter'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import logger from 'redux-logger'
+import reducers from './reducer'
 
-export default {
-    counter
-}
+const store = createStore(reducers, applyMiddleware(thunk, logger))
+export default store
