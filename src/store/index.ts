ee6d6a3eaea226425012a805c625e32cda29aaeb
zhuyuanmin <13058039407@163.com>
2020-08-13 10:46:29 +0800

    Delete index.ts

--- src/store/index.ts
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux'
-import * as LoginReducer from './user'
-export const dispatchs = {
-    ...LoginReducer,
-}
-
-export default combineReducers({
-    login: LoginReducer.default,
-})
