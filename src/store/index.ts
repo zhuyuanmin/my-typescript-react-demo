c4b621bb8429e423eb88ba85fa4a43ccbe39613a
zhuyuanmin <13058039407@163.com>
2020-08-20 17:19:51 +0800

    Update index.ts

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,5 +3,13 @@ import thunk from 'redux-thunk'
 import logger from 'redux-logger'
 import reducers from './reducer'
 
-const store = createStore(reducers, applyMiddleware(thunk, logger))
+const NODE_ENV = process.env.NODE_ENV
+let store: any = null
+
+if (NODE_ENV === 'development') {
+    store = createStore(reducers, applyMiddleware(thunk, logger))
+} else {
+    store = createStore(reducers, applyMiddleware(thunk))
+}
+
 export default store
